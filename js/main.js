84edc52bf21fc76f928b615d0b5ab93f4eefd286
송현우 <101445027+googoo81@users.noreply.github.com>
2022-09-13 20:41:08 +0900

    scroll animetion

--- js/main.js
+++ js/main.js
@@ -15,3 +15,24 @@ searchInputEl.addEventListener('blur', function () {
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder', '');
 });
+
+
+const badgeEl = document.querySelector('header .badges')
+
+window.addEventListener('scroll', _.throttle(function () {
+    if (window.scrollY > 500) {
+        //배지 숨기기
+        // gsap.to(요소, 지속시간, 옵션);
+        gsap.to(badgeEl, .6, {
+            opacity: 0,
+            display: 'none'
+        })
+    } else {
+        //배지 보이기
+        gsap.to(badgeEl, .6, {
+            opacity: 1,
+            display: 'block'
+        })
+    }
+}, 300))
+// _.throttle(함수, 시간(단위: ms))
