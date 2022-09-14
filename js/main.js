b9fb2c99b0a226b7c13297dee73e25459a972a0e
송현우 <101445027+googoo81@users.noreply.github.com>
2022-09-14 12:45:46 +0900

    promotion imge slide

--- js/main.js
+++ js/main.js
@@ -54,3 +54,20 @@ new Swiper('.notice-line .swiper-container', {
     autoplay: true,
     loop: true
 });
+new Swiper('.promotion .swiper-container', {
+    slidesPerView: 3, //한번에 보여줄 슬라이드 개수
+    spaceBetween: 10, //슬라이드 사이의 여백
+    centeredSlides: true, //1번 슬라이드가 가운데 보이기
+    loop: true,
+    autoplay: {
+        delay: 5000
+    },
+    pagination: {
+        el: '.promotion .swiper-pagination', //페이지 번호 요소 선택자
+        clickable: true //사용자의 페이지 번호 요소 제어
+    },
+    navigation: {
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next'
+    }
+});
