bda273d45f436b4c46b387e4dc161c140691641e
송현우 <101445027+googoo81@users.noreply.github.com>
2022-09-14 09:04:07 +0900

    sequential animetion

--- js/main.js
+++ js/main.js
@@ -36,3 +36,13 @@ window.addEventListener('scroll', _.throttle(function () {
     }
 }, 300))
 // _.throttle(함수, 시간(단위: ms))
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+    // gsap.to(요소, 지속시간, 옵션);
+    gsap.to(fadeEl, 1, {
+        delay: (index + 1) * .7, // 0.7, 1.4, 2.1, 2.8
+        opacity: 1
+    });
+});
