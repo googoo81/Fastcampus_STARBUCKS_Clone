e7392828b26e3b00e7e559ae650e7557f7b074ba
송현우 <101445027+googoo81@users.noreply.github.com>
2022-09-14 11:07:50 +0900

    Notice block with swiper js

--- js/main.js
+++ js/main.js
@@ -46,3 +46,11 @@ fadeEls.forEach(function (fadeEl, index) {
         opacity: 1
     });
 });
+
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+    direction: 'vertical',
+    autoplay: true,
+    loop: true
+});
