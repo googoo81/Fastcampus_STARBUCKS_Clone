d86414a7dbe95a3965e5edfb328e4596b6943238
송현우 <101445027+googoo81@users.noreply.github.com>
2022-09-17 23:50:29 +0900

    multiple El slide

--- js/main.js
+++ js/main.js
@@ -71,6 +71,16 @@ new Swiper('.promotion .swiper-container', {
         nextEl: '.promotion .swiper-next'
     }
 });
+new Swiper('.awards .swiper-container', {
+    autoplay: true,
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 5,
+    navigation: {
+        prevEl: '.awards .swiper-prev',
+        nextEl: '.awards .swiper-next'
+    }
+});
 
 
 const promotionEl = document.querySelector('.promotion');
